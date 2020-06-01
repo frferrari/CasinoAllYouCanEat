9409bfa49a880438133448147b6dce4d56868204
Francois <f.ferrari@wanadoo.fr>
2020-06-02 00:54:00 +0100

    Added tests

--- src/test/scala/RestaurantTest.scala
+++ src/test/scala/RestaurantTest.scala
@@ -15,7 +15,7 @@ class RestaurantTest extends WordSpec with Matchers {
       }
     }
 
-    "given 2 guests for lunch and 1 guest queuing/leaving without waiting for a free seat" should {
+    "given 2 guests for lunch and 1 guest queuing/leaving without eating" should {
       "serve this 2 guests" in {
         val state: State = restaurant.compute(Array(10, 15, 20), Array(1, 2, 3, 3, 2, 1))
 
@@ -29,6 +29,28 @@ class RestaurantTest extends WordSpec with Matchers {
     "given 3 guests for lunch and 1 guest queuing/waiting for a free seat" should {
       "serve this 3 guests" in {
         val state: State = restaurant.compute(Array(10, 15, 20), Array(1, 2, 3, 2, 3, 1))
+
+        state.totalPayed shouldEqual 45
+        state.eatingGuests.isEmpty shouldEqual true
+        state.haveBeenEatingGuests should contain theSameElementsAs List(1, 2, 3)
+        state.queuingGuests.isEmpty shouldEqual true
+      }
+    }
+
+    "given 4 guests for lunch and 2 guests queuing/waiting for a free seat" should {
+      "serve this 4 guests" in {
+        val state: State = restaurant.compute(Array(10, 15, 20, 15), Array(1, 2, 3, 4, 2, 3, 4, 1))
+
+        state.totalPayed shouldEqual 60
+        state.eatingGuests.isEmpty shouldEqual true
+        state.haveBeenEatingGuests should contain theSameElementsAs List(1, 2, 3, 4)
+        state.queuingGuests.isEmpty shouldEqual true
+      }
+    }
+
+    "given 4 guests for lunch and 1 guest queuing/waiting for a free seat and 1 guest queuing/leaving without eating" should {
+      "serve this 4 guests" in {
+        val state: State = restaurant.compute(Array(10, 15, 20, 15), Array(1, 2, 3, 4, 4, 2, 3, 1))
 
         state.totalPayed shouldEqual 45
         state.eatingGuests.isEmpty shouldEqual true
